7e39cfffcbe5cc60e438e579fe866acb81530367
Nicke96 <56563668+Nicke96@users.noreply.github.com>
2025-03-04 13:46:26 +0000

    Test

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -18,7 +18,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyDw4NBjik1EGWMoioYP0-foQn4ejQpNYOQ",
 
   authDomain: "vue-site-b0745.firebaseapp.com",
 
